7b7a498b235db4e57336d1e345c08fde263521af
mwakok <m.w.a.kok@tudelft.nl>
2023-04-06 20:43:13 +0200

    Add matlab buildfile

---
+++ buildfile.m
@@ -0,0 +1,33 @@
+function plan = buildfile
+    plan = buildplan(localfunctions);
+    plan.DefaultTasks = "compile";
+    plan("compile").Dependencies = ["check" "test"];
+end
+
+
+function checkTask(~)
+% Identify code issues
+issues = codeIssues;
+assert(isempty(issues.Issues),formattedDisplayText( ...
+    issues.Issues(:,["Location" "Severity" "Description"])))
+end
+
+
+function testTask(~)
+% Run unit tests
+results = runtests(IncludeSubfolders=true,OutputDetail="terse");
+assertSuccess(results);
+end
+
+
+function compileTask(~)
+% Compile Python package
+appFiles = {'callFunction.m', 'callMethod.m', 'loadMatlabVars.m', 'saveMatlabVars.m'};
+f = fullfile('src', 'matlab', appFiles);
+opts = compiler.build.PythonPackageOptions(f,...
+    'OutputDir','PythonWrapper',...
+    'AutoDetectDataFiles','off',...
+    'Verbose','on');
+compiler.build.pythonPackage(opts);
+end
+
